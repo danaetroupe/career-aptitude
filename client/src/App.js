27ade1b666420e98f9ed71da5f13c9eda7549035
danaetroupe <danaetroupe@gmail.com>
2025-04-02 13:26:17 -0400

    Client Init

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import { Provider } from "@/components/ui/provider";
+import Home from './components/Home';
+
+function App({ Component, pageProps }) {
+  return (
+    <Provider>
+      <Component {...pageProps} />
+      <Home/>
+    </Provider>
+  )
+}
+
+export default App;
